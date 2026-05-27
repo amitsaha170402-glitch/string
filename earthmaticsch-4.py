952409a14ddef716b59980246f545289ea355b2c
amitsaha170402-glitch <amitsaha170402@gmail.com>
2026-05-27 11:04:06 +0530

    Update earthmaticsch-4.py

--- earthmaticsch-4.py
+++ earthmaticsch-4.py
@@ -3,14 +3,20 @@ candy=10
 
 #candy= candy +1 (add)
 candy += 1
+print(candy)
 #candy= candy *3 (multiply)
 candy *=3
+print(candy)
 #candy= candy/4 (divide)
 candy /=4
+print(candy)
 #candy=candy %3 (remainder)
 candy %=3
+print(candy)
 #candy=candy**3  (power)
 candy **=3
+print(candy)
+
 candy= candy//3
 
 
