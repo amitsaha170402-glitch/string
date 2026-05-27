0e8589b32ae3267cc3df8b8ceb21b9b08c321b58
amitsaha170402-glitch <amitsaha170402@gmail.com>
2026-05-27 10:53:07 +0530

    Add files via upload
    type casting

---
+++ chp2_typecasting.py
@@ -0,0 +1,22 @@
+#typecasting = is the process of converting a veriable from one data to another str(),float(),int() and bool()
+
+
+#example
+name="hay bro "
+age= 26
+gpa=3.4
+is_student = True
+yourid=3
+#data type of the value
+
+print(type(age))
+print(type (is_student))
+
+#typecasting
+name=bool(name)
+print(name)
+
+age=float(age)
+print(age)
+
+ 
