379365628121030e7aec1381b6f23dd888113d7b
amitsaha170402-glitch <amitsaha170402@gmail.com>
2026-05-27 11:06:06 +0530

    Restore arithmetic operations and print results
    Reintroduced various arithmetic operations with rounded results and print statements.

--- 3arthmaticpart_2.py
+++ 3arthmaticpart_2.py
@@ -3,12 +3,24 @@ y=4.6
 z=5
 
 
-#result= round(x)
-#result= abs(y)
-#result= pow(z, 3)
-#result= min(x,y,z)
-#result= max(x,y,z)
-#result= round(y)
+result= round(x)
+print(round(result ,2))
+
+result= abs(y)
+print(round(result ,2))
+
+result= pow(z, 3)
+print(round(result ,2))
+
+result= min(x,y,z)
+print(round(result ,2))
+
+result= max(x,y,z)
+print(round(result ,2))
+
+result= round(y)
+print(round(result ,2))
+
 result =x*y 
 
 print(round(result ,2))
