1e91c744c54d3c7616fb7b823d3922cafeb07292
amitsaha170402-glitch <amitsaha170402@gmail.com>
2026-05-27 10:55:52 +0530

    Uncomment mathematical expressions in earthmaticsch-4.py
    Uncomment mathematical operations for variable 'candy'.

--- earthmaticsch-4.py
+++ earthmaticsch-4.py
@@ -2,15 +2,15 @@
 candy=10
 
 #candy= candy +1 (add)
-#candy += 1
+candy += 1
 #candy= candy *3 (multiply)
-#candy *=3
+candy *=3
 #candy= candy/4 (divide)
-#candy /=4
+candy /=4
 #candy=candy %3 (remainder)
-#candy %=3
+candy %=3
 #candy=candy**3  (power)
-#candy **=3
+candy **=3
 candy= candy//3
 
 
