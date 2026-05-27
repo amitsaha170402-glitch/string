793b4b4164840eed39c02c0fef16b53455cb0abe
amitsaha170402-glitch <amitsaha170402@gmail.com>
2026-05-27 10:49:37 +0530

    Add files via upload
    my first python file

---
+++ chapter1.py
@@ -0,0 +1,49 @@
+print('Hello world')
+print("there are you")
+print('how are you')
+#veriables = a container
+# strings=which store words 
+first_name="bro"
+email="amit@123gmail.com"
+food="pizza"
+
+print(f"hello  {first_name}" )
+print(f"your mail is {email}")
+
+#integers=stores number
+age=25
+quantity=5
+
+print(f"your age is {age}")
+print(f"you ae buying {quantity} of icecream")
+
+#flote=price i the sence of number  and decimal portion
+
+price=110.99
+gpa=3.2
+print(f"the price is {price}$")
+print(f"your gpa is {gpa}")
+
+
+
+#boolean=true or false
+is_student=False
+if is_student:
+    print("you are student")
+else:
+    print("you are not")
+
+print(gpa)
+
+
+message="hello world "
+
+new_message=message.replace("world","void")
+
+
+
+print(new_message[0:])
+print (message.find("world"))
+
+
+
