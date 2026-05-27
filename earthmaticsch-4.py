49f43f0c81cc4cada8a56707c67a87a67e64c01b
amitsaha170402-glitch <amitsaha170402@gmail.com>
2026-05-27 10:54:57 +0530

    Add files via upload

---
+++ earthmaticsch-4.py
@@ -0,0 +1,19 @@
+#some basic methamatical expration
+candy=10
+
+#candy= candy +1 (add)
+#candy += 1
+#candy= candy *3 (multiply)
+#candy *=3
+#candy= candy/4 (divide)
+#candy /=4
+#candy=candy %3 (remainder)
+#candy %=3
+#candy=candy**3  (power)
+#candy **=3
+candy= candy//3
+
+
+
+
+print(candy)
